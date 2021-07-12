d4e4563c364d863cc191c1b8e368c2422a63bd5b
CesarALuna <CesLun@gmail.com>
2021-07-12 14:16:42 -0700

    project mostly finished, needs some styling

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header>
+      <p>Meme Machine</p>
+
+      <img
+        src="https://www.pngall.com/wp-content/uploads/5/Meme-Troll-Transparent.png"
+        alt="problem? :D"
+      />
+    </header>
+  )
+}
+export default Header
